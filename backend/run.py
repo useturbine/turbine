0f5e0f57b3d3b5e8f03f7fab5948771415c9b6a9
Ankit Solanki <ankit03june@gmail.com>
2023-07-18 04:21:18 +0530

    zip old code

--- backend/run.py
+++
@@ -1,3 +0,0 @@
-from src.core import PastThreads
-
-PastThreads().get_by_email("ankit03june@gmail.com")
--- webapp/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, Center } from "@mantine/core";
-
-export default function Home() {
-  return (
-    <Center m="lg">
-      <Button variant="outline">Login with Google</Button>
-    </Center>
-  );
-}
