710fdf7e7c711ae8208e4457719123802f4c4f57
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-07-20 17:06:38 +0530

    Changes

--- backend/src/agent.py
+++ backend/src/agent.py
@@ -6,14 +6,44 @@ from langchain.text_splitter import CharacterTextSplitter
 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from config import OPENAI_API_KEY
+
+from langchain.agents import Tool
+from langchain.agents import AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import initialize_agent
+
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.llms.openai import OpenAI
+from langchain.agents import AgentExecutor
+from langchain.agents.agent_types import AgentType
+from langchain.chat_models import ChatOpenAI
 
 
-import os
-from dotenv import load_dotenv
+db = SQLDatabase.from_uri("sqlite:///./database.db")
+toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
 
-load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+memory = ConversationBufferMemory(memory_key="chat_history")
+llm = OpenAI(temperature=0)
+
+agent = initialize_agent(
+    llm=OpenAI(temperature=0),
+    tools=toolkit.get_tools(),
+    verbose=True,
+    memory=memory,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+
+answer = agent.run(
+    input="My name is Sumit. Give me my total AWS cost in the past week."
+)
+print(answer)
 
 
 class DocsData:
@@ -85,7 +115,3 @@ class PromptSystem:
 
     def ask(self, topic):
         return self.chain.run({"context": self.context, "topic": topic})
-
-
-resp = PromptSystem().ask()
-print(resp)
