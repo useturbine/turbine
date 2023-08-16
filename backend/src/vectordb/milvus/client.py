29bc3827cab649d28ceb57ee8b6f6957a3f36d07
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-08-16 17:17:31 +0530

    gitignore and black format

--- backend/src/vectordb/milvus/client.py
+++ backend/src/vectordb/milvus/client.py
@@ -1,13 +1,16 @@
 from pymilvus import Collection
 from pymilvus import MilvusClient
+
 # from MilvusException import CollectionNotExistException
 
-class Client():
+
+class Client:
     def __init__(self, **kwargs):
         self.client = MilvusClient(
-            uri=kwargs.get('uri'),
-            user=kwargs.get('user'),
-            password=kwargs.get('password'))
+            uri=kwargs.get("uri"),
+            user=kwargs.get("user"),
+            password=kwargs.get("password"),
+        )
 
     def create_collection(self, name, params):
         return self.client.create_collection(name, params)
@@ -20,5 +23,7 @@ class Client():
         self.client.insert(collection, params)
 
     def search(self, collection, field, data, top_k, params):
-        results = Collection(collection).search(data=data, anns_field=field, limit=top_k, param=params)
+        results = Collection(collection).search(
+            data=data, anns_field=field, limit=top_k, param=params
+        )
         return [h for h in results[0]]
