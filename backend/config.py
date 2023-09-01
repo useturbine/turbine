5e071817d7e42fef429708dfb53493e69203a580
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-09-01 13:33:30 +0530

    Make redpanda listen to both external and internal connections

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ class Config:
     postgres_url = os.getenv(
         "POSTGRES_URL", "postgres://admin:passwd@turbine-postgres:5432/turbine"
     )
-    kafka_url = os.getenv("KAFKA_URL", "localhost:9092")
+    kafka_url = os.getenv("KAFKA_URL", "localhost:19092")
     debezium_url = os.getenv("DEBEZIUM_URL", "http://localhost:8083")
     openai_api_key = os.getenv("OPENAI_API_KEY", "")
     milvus_url = os.getenv("MILVUS_URL", "tcp://localhost:19530")
