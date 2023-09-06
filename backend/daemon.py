6015b8012dc7f0d793b1982da7607adf9e5736a2
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-09-06 13:09:21 +0530

    INFO log level on daemon

--- backend/daemon.py
+++ backend/daemon.py
@@ -2,7 +2,7 @@ from src.daemon.daemon import Daemon
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] [%(name)s]: %(message)s",
 )
 
