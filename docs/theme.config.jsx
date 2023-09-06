01dcd3eddd0913a25f02a5fae4507de9abf90545
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-09-06 22:15:13 +0530

    Some changes in docs and landing

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -50,7 +50,18 @@ export default {
       />
       <meta property="og:url" content="https://useturbine.com" />
       <title>Turbine: Develop LLM features on top of your data</title>
-      <link rel="icon" type="image/svg" href="/images/turbine.svg" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
     </>
   ),
   feedback: { content: null },
