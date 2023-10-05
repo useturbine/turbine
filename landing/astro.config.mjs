8c417ed01991f067a443d0408d7c708724e6dba4
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-10-05 21:24:31 +0530

    Landing page overhaul

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,7 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { autolinkConfig } from "./plugins/rehype-autolink-config";
+import rehypeSlug from "rehype-slug";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+	site: "https://astros.zank.studio",
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [tailwind(), sitemap(), alpinejs()],
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+	},
 });
