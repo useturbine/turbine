1a5dc27476d1a7785a4b152c026f7622e735db19
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-10-17 15:13:51 +0530

    Fix typo

--- backend/turbine/api/app.py
+++ backend/turbine/api/app.py
@@ -36,7 +36,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173, https://console.useturbine.com"],
+    allow_origins=["http://localhost:5173", "https://console.useturbine.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
