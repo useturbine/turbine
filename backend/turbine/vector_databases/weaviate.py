d2a7e43d3c7cea3adf9f4b1162aa583edab11305
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-10-30 18:16:22 +0530

    Start adding weaviate

---
+++ backend/turbine/vector_databases/weaviate.py
@@ -0,0 +1,13 @@
+from turbine.vector_databases import VectorDatabase
+from weaviate import Client, AuthApiKey
+
+
+class WeaviateVectorDB(VectorDatabase):
+    url: str
+    api_key: str
+
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+        self.client = Client(
+            url=self.url, auth_client_secret=AuthApiKey(api_key=self.api_key)
+        )
